e5071ad61b8cbd97a230290debedfe3ca6d4352b
Ana Paula Pereira <ana.pereira014@edu.pucrs.br>
2026-04-11 22:42:40 +0000

    exercicio 7 finalizado

--- src/ex7.java
+++ src/ex7.java
@@ -3,37 +3,59 @@ public class ex7 {
     // Assumindo os Custos: Remoção=R, Inserção=I , Substituição=S e Match=M=0;
     static int iteracoes = 0;
 
-    public static int distEdProgDina(String A, String B){
+    public static int distanciaDeEdicaoPD(String A, String B){
 
-        int m = A.length();
-        int n = B.length();
+      int m = A.length();
+      int n = B.length();
 
-        int [][]matriz = new int[m+1][n+1];
+      int [][]matriz = new int[m+1][n+1];
 
-        matriz[0][0] = 0;
+      matriz[0][0] = 0;
 
-        for (int i = 1; i < m; i++){
-            
-        }
+      for (int i = 1; i < m; i++) {
+         matriz[i][0] = matriz[i - 1][0] + 1;
+         iteracoes++;
+      }
 
-        // arrumar a partir daqui
-        Para i = 1 até m
-           matriz[i][0] = matriz[i-1][0] + 1  // soma uma I;
-        Para j = 1 até n
-           matriz[0][j] = matriz[0][j-1] + 1  // Soma uma R;
-        Para i = 1 até m
-           Para j = 1 até n
-              Se A[i] == B[j]
-             custoExtra = 0 //Operação M;
-              Senão
-             custoExtra = 1 //Operação S;
-              matriz[i][j] = Mínimo(matriz[i-1][j] +1, matriz[i][j-1] +1, 
-                        matriz[i-1][j-1] + custoExtra];
-        devolva matriz[m][n];
-    }
+      for (int j = 1; j <= n; j++) {
+         matriz[0][j] = matriz[0][j - 1] + 1;
+         iteracoes++;
+      }
+
+      // preenchimento da matriz
+      for (int i = 1; i <= m; i++) {
+         for (int j = 1; j <= n; j++){
+            iteracoes++;
+
+            int custoExtra;
+
+            if (A.charAt(i - 1) == B.charAt(j - 1)) {
+               custoExtra = 0; // match
+            } else {
+               custoExtra = 1; // substituição
+            }
+
+            matriz[i][j] = Math.min(
+               Math.min(matriz[i - 1][j] + 1, //remocao
+                   matriz[i][j - 1] + 1), //insercao
+               matriz[i - 1][j - 1] + custoExtra //substituicao/match
+            );
+         }
+      }
+      return matriz[m][n];
+   }
 
     public static void main(String [] args){
 
-    }
+      String A = "casa";
+      String B = "pai";
+
+      int distancia = distanciaDeEdicaoPD(A, B);
+
+      System.out.println("String A: " + A);
+      System.out.println("String B: " + B);
+      System.out.println("Distância de edição: " + distancia);
+      System.out.println("Número de iterações: " + iteracoes);
+   }
     
 }
